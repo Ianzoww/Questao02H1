804c150e05ffc129101dbdc57dd1550d4b45e0c5
Ianzoww <zeroxp534@gmail.com>
2024-08-14 19:56:58 -0300

    Primeira Versao

---
+++ Questao2/Controllers/ProdutoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Questao2.Modells;
+
+namespace Questao2.Controllers
+{
+    [ApiController]
+    [Route("API/Frete")] //CONTROLLER
+    public class ProdutoController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CalcularFreteProduto(Frete frete)
+        {
+            double volume = frete.Altura * frete.Largura * frete.Comprimento;
+            
+            
+            const double taxaPorCmCubico = 0.01;
+
+            double taxaPorEstado;
+            switch (frete.UF.ToUpper())
+            {
+                case "SP":
+                    taxaPorEstado = 50;
+                    break;
+
+                case "RJ":
+                    taxaPorEstado = 60;
+                    break;
+
+                case "MG":
+                    taxaPorEstado = 55;
+                    break;
+
+                default:
+                    taxaPorEstado = 70;
+                    break;
+            }
+            double Resultado = (volume * taxaPorCmCubico) + taxaPorEstado;
+            string resultadoEmReais = Resultado.ToString("C2", new System.Globalization.CultureInfo("pt-BR"));
+
+            return Ok(resultadoEmReais);
+          
+        }
+    }
+}
---
+++ Questao2/Modells/Frete.cs
@@ -0,0 +1,12 @@
+﻿namespace Questao2.Modells
+{
+    public class Frete
+    {
+        public string Nome { get; set; }
+        public float Peso { get; set; }
+        public float Altura { get; set; }
+        public float Largura { get; set; }
+        public float Comprimento { get; set; }
+        public string UF { get; set; }
+    }
+}
